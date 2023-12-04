7623074f2df9918edb5a427643ba3b1b5e2716b3
Kristy Martinez <kristyvanderhill@gmail.com>
2023-12-03 19:04:14 -0600

    Merge Project Documents

---
+++ Media_Sphere/PdfDocumentReader.cs
@@ -0,0 +1,34 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Text;
+
+namespace Media_Sphere
+{
+    public class PdfDocumentReader
+    {
+        public static string ReadTextFromPdfDocument(string filePath)
+        {
+            StringBuilder text = new StringBuilder();
+
+            try
+            {
+                using (PdfReader reader = new PdfReader(filePath))
+                {
+                    for (int i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                    }
+                }
+
+                return text.ToString();
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                Console.WriteLine("Error reading PDF document: " + ex.Message);
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ Media_Sphere/WordDocumentReader.cs
@@ -0,0 +1,61 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Text;
+
+namespace Media_Sphere
+{
+    public class WordDocumentReader
+    {
+        public static string ReadTextFromWordDocument(string filePath)
+        {
+            StringBuilder textBuilder = new StringBuilder();
+
+            try
+            {
+                using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(filePath, false))
+                {
+                    Body body = wordDoc.MainDocumentPart.Document.Body;
+                    if (body != null)
+                    {
+                        foreach (OpenXmlElement element in body.Elements())
+                        {
+                            if (element is Paragraph)
+                            {
+                                textBuilder.AppendLine(GetParagraphText((Paragraph)element));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                Console.WriteLine("Error reading Word document: " + ex.Message);
+            }
+
+            return textBuilder.ToString();
+        }
+
+        private static string GetParagraphText(Paragraph paragraph)
+        {
+            StringBuilder paragraphText = new StringBuilder();
+            foreach (OpenXmlElement element in paragraph.Elements())
+            {
+                if (element is Run)
+                {
+                    foreach (Text text in element.Descendants<Text>())
+                    {
+                        paragraphText.Append(text.Text);
+                    }
+                }
+                else if (element is Break)
+                {
+                    paragraphText.AppendLine();
+                }
+            }
+            return paragraphText.ToString();
+        }
+    }
+}
