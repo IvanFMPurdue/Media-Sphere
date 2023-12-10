42fdab99c0deaa1646076d54f131e8b23c74ede0
Robert <your.email@example.com>
2023-12-09 19:50:52 -0600

    JPEG, GIF, PNG Readers

---
+++ TextDocumentReader.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Media_Sphere
+{
+    public class TextDocumentReader
+    {
+        public static string ReadTextFromFile(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+    }
+}
